96c9e064e628604c372c29be21a71ee9a9140f7d
manuel-sunol <msunol@itba.edu.ar>
2025-10-15 15:29:24 -0300

    memory manager basico terminado

--- x64BareBones/Kernel/include/pmem.h
+++ x64BareBones/Kernel/include/pmem.h
@@ -10,7 +10,7 @@ typedef struct {
     uint64_t free;
 } pm_stats_t;
 
-void  pm_init(void *pool_start, size_t pool_len);
-void *pm_alloc(size_t bytes);
-void  pm_free(void *ptr);
-void  pm_get_stats(pm_stats_t *out);
+int  mem_init(void *pool_start, size_t pool_len);
+void *mem_alloc(size_t bytes);
+void  mem_free(void *ptr);
+void  mem_get_stats(pm_stats_t *out);
